f6ba7e878fda0898991b835623fd9b382dc57db6
= <siheonl@andrew.cmu.edu>
2020-02-11 17:40:27 -0500

    added validations, relationships, scopes, and name method. Tests pass

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,16 @@
 class Child < ApplicationRecord
-    
+		has_many :chores
+		has_many :tasks, through: :chores
+
+		validates_presence_of :first_name
+		validates_presence_of :last_name
+
+		scope :alphabetical, -> { order('last_name', 'first_name') }
+		scope :active, -> { where('active = ?', true) }
+
+
+		def name
+			return self.first_name + " " + self.last_name
+		end
+
 end
